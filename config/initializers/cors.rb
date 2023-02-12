20bdf246c7fbcd788ef1ce9fafeb203a8024d5ad
Leonardo Albornoz <albornozleo95@gmail.com>
2023-02-12 07:04:43 -0400

    Add cors to frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "renturtech.netlify.app"
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'renturtechrender.netlify.app'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
